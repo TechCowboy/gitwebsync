ccdaf5eb35e4d6ddfe0efda52569ecde424b02df
Norman Davie <ecompservices@gmail.com>
2023-07-04 14:51:27 -0300

    if only one commit ahead it would miss it

--- gitwebsync.py
+++ gitwebsync.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+print("In Progress...")
 account = "https://github.com/TechCowboy"
 URL = account + "?tab=repositories"
 page = requests.get(URL)
@@ -29,7 +30,10 @@ for repository in repository_links:
     position =  result.find("This branch is up to date")
     if position == -1:
         position = result.find("commits behind")
+        if position == -1:
+            position = result.find("commit behind")
         if position != -1:
+            print("Needs Sync " + repository)
             repositories_to_update.append(repository)
         else:
             print("Newer      " + repository)
@@ -37,10 +41,11 @@ for repository in repository_links:
             print("Up to date " + repository)
         
         
-
+print()
 if len(repositories_to_update) == 0:
     print("Everything is up to date.")
 else:
     print("need to update:")
     for repository in repositories_to_update:
         print(repository)
+        
